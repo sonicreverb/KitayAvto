40d4fbf45af71fc1b36531c88f29cbd640668300
Vladislav <vladislav.krasivin@gmail.com>
2023-11-20 17:50:03 +0300

    Driver timeout handled.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import datetime
 import threading
 import multiprocessing
+import time
+
 import data_parser.parser as parser
 import database as db
 import logs
@@ -30,6 +32,28 @@ def activity_validation():
     logs.log_info(f'[ACTIVITY VALIDATION] ({threading.current_thread().name}) Activity validation complete.')
 
 
+TIMEOUT_SECONDS = 200
+
+
+# функция для запуска в отдельном потоке, которая проверяет находиться ли драйвер больше чем duration секунд
+# на одной и той же странице, в случае если драйвер "завис" на одной и той же странице - вызывает driver.close()
+def timeout_validation(driver, duration=TIMEOUT_SECONDS):
+    old_url = driver.current_url
+    time.sleep(duration)
+
+    if driver.current_url == old_url:
+        '''print(f'[TIMEOUT VALIDATION] ({threading.current_thread().name}) '
+             f'Driver was closed as the page processing time exceeded. ({old_url})') '''
+
+        logs.log_warning(f'[TIMEOUT VALIDATION] ({threading.current_thread().name}) '
+                         f'Driver was closed as the page processing time exceeded. {old_url}')
+        raise TimeoutError("Driver timeout")
+    else:
+        logs.log_warning(f'[TIMEOUT VALIDATION] ({threading.current_thread().name}) '
+                         f'The driver successfully processed the page within the specified time. '
+                         f'{old_url}')
+
+
 # получение ссылок на товары, находящиеся в categories_urls_files, их парсинг и запись в БД
 def parsing_process(categories_urls_file, products_urls_file, proxy_data):
     parser.get_target_urls(categories_urls_file, products_urls_file, proxy_data=proxy_data)
@@ -57,8 +81,15 @@ def parsing_process(categories_urls_file, products_urls_file, proxy_data):
         for url in urls_to_parse:
             try:
                 print(f'[PARSING PROCESS] ({current_process_name}) {successful_prods_count}/{total_products_num} '
-                      f'Proseccing product with URL: {url}')
+                      f'Processing product with URL: {url}')
                 driver.execute_script('window.location.href = arguments[0];', url)
+
+                # отдельный поток для перехода на новый товар, если текущий обрабатывается уже более TIMEOUT_SECONDS
+                timeoutValThread = threading.Thread(target=timeout_validation, name='TimeoutValidationThread',
+                                                    args=(driver, TIMEOUT_SECONDS))
+                timeoutValThread.start()
+                timeout_validation(driver, TIMEOUT_SECONDS)
+
                 product_data = parser.get_data(driver, url)
 
                 if product_data:
@@ -72,8 +103,16 @@ def parsing_process(categories_urls_file, products_urls_file, proxy_data):
                     invalid_links_file.write(url + "\n")
                     invalid_links_file.close()
                     total_products_num -= 1
+            except TimeoutError:
+                total_products_num -= 1
+                print('[PARSING PROCESS] Driver timeout.')
+                logs.log_info('[PARSING PROCESS] Driver timeout.')
+                print('[PARSING RPOCESS] Recreating driver.')
+                logs.log_info('[PARSING RPOCESS] Recreating driver.')
+                driver = parser.create_driver(images_enabled=True, proxy_data=proxy_data)
+                driver.get('https://duckduckgo.com')
             except Exception as ex:
-                print(ex)
+                print(f'[PARSING PROCESS] Driver was emergency closed ({ex})')
                 # если ошибочно закрылись все окна браузера или сам браузер был аварийно закрыт, начинаем новую сессию
                 if len(driver.window_handles) == 0 or driver is None or driver.service.process is None:
                     print('[PARSING RPOCESS] Recreating driver.')
@@ -122,7 +161,7 @@ if __name__ == '__main__':
 
         parserProcess1.start()
         parserProcess2.start()
-        parserProcess3.start()
+        # parserProcess3.start()
 
         activValThread.join()
         print('Обновление активности завершено.')
@@ -133,7 +172,7 @@ if __name__ == '__main__':
         print('Первый процесс парсинга завершён.')
         parserProcess2.join()
         print('Второй процесс парсинга завершён.')
-        parserProcess3.join()
+        # parserProcess3.join()
         print('Третий процесс парсинга завершён.')
         logs.log_info('Парсинг завершён.')
 
@@ -141,7 +180,7 @@ if __name__ == '__main__':
 
         parser.delete_tmp_imgs()
         tables.write_data_to_xlsx('SELECT * FROM vehicles_data;', 'KitayAvto_output.xlsx')
-        tables.upload_file_to_ftp('KitayAvto_output.xlsx')
+        # tables.upload_file_to_ftp('KitayAvto_output.xlsx')
         end_session_time = datetime.datetime.now()
 
         send_notification(f'[KITAY AVTO] Данные успешно отправлены на FTP! {datetime.datetime.now()}')
